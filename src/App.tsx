9ed6570d0f3da23d3888aa6095b4376ded9ed11f
Atanas Roussev <atanas@nextinterfaces.com>
2025-04-08 10:36:50 +0300

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,9 @@
-import { useState } from 'react';
-import { useGetUsersQuery, useCreateUserMutation } from './generated-queries';
 
 function App() {
-  // const { data: users, refetch } = useGetUsersQuery();
-  // const { mutate: createUser } = useCreateUserMutation();
-
-  // const handleCreateUser = () => {
-  //   createUser({ id: Date.now(), name: 'New User' });
-  // };
 
   return (
     <>
-      <div className="card">111
-        {/*<button onClick={refetch}>*/}
-        {/*  Get Users*/}
-        {/*</button>*/}
-        {/*<button onClick={handleCreateUser}>*/}
-        {/*  Create User*/}
-        {/*</button>*/}
-        {/*<div>*/}
-        {/*  {users && users.map(user => (*/}
-        {/*    <div key={user.id}>{user.name}</div>*/}
-        {/*  ))}*/}
-        {/*</div>*/}
+      <div className="card">
       </div>
     </>
   );
